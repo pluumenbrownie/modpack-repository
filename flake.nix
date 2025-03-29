2bc9f4b6dd0c7ead363bbf82f2f6c583ca90ad86
pluumenbrownie <git.wessel@beumertjes.nl>
2025-03-29 02:11:17 +0100

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Flake used for computational science. Shows how to add python packages to config when unavailable.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        toml-cli
+      ];
+    };
+
+    doCheck = false;
+  };
+}
