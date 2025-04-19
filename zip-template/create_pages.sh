2c9428bf4b40a0ae94a8490c8ca280c041fb48b0
pluumenbrownie <git.wessel@beumertjes.nl>
2025-04-19 16:56:15 +0200

    cleaned up and added comments

--- zip-template/create_pages.sh
+++ zip-template/create_pages.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 
-echo '[book]\ntitle = "Pluumenbrownies Modpack Repository"' >book.toml
+# Write the metadata file book.toml
+echo '[book]' >book.toml
+echo 'title = "Pluumenbrownies Modpack Repository"' >>book.toml
 cat book.toml
-echo '[Overview](overview.md)' >src/SUMMARY.md
 
+# Write the SUMMARY.md file and automatically find the
+echo '[Overview](overview.md)' >src/SUMMARY.md
 find . -type d -exec test -e '{}'/pack.toml \; \
     -exec sh -c 'echo "- [$(toml get $1/pack.toml name)]($1.md)" >>src/SUMMARY.md' sh {} \; \
     -exec sh -c 'echo $1 >src/$1.md' sh {} \; \
     -exec sh -c 'echo $1' sh {} \; \
     -exec sh -c 'cat $1.md' sh {} \;
-
-echo 'Something overviewing' >src/overview.md
-
 cat src/SUMMARY.md
+
+# Create overview page in overview.md
+echo 'Something overviewing' >src/overview.md
