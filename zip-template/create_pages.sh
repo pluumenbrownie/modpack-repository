30484d792932b639c4ba7375bcfe19e9cf6d5620
pluumenbrownie <git.wessel@beumertjes.nl>
2025-04-19 16:42:54 +0200

    added debugging prints

--- zip-template/create_pages.sh
+++ zip-template/create_pages.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
 echo -e '[book]\ntitle = "Pluumenbrownies Modpack Repository"' >book.toml
+cat book.toml
 echo -e '[Overview](overview.md)' >src/SUMMARY.md
 
 find . -type d -exec test -e '{}'/pack.toml \; \
     -exec sh -c 'echo -e "- [$(toml get $1/pack.toml name)]($1.md)" >>src/SUMMARY.md' sh {} \; \
     -exec sh -c 'echo -e $1 >src/$1.md' sh {} \; \
-    -exec sh -c 'echo $1' sh {} \;
+    -exec sh -c 'echo $1' sh {} \; \
+    -exec sh -c 'cat $1.md' sh {} \;
 
 echo -e 'Something overviewing' >src/overview.md
+
+cat src/SUMMARY.md
