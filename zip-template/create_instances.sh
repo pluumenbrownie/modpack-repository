b1d3ca81772ae125d6ba992e4800ffb16549fa7b
pluumenbrownie <git.wessel@beumertjes.nl>
2025-04-20 21:54:38 +0200

    fixed url not getting replaced correctly

--- zip-template/create_instances.sh
+++ zip-template/create_instances.sh
@@ -36,17 +36,16 @@ set_mmc_pack() {
 export -f set_mmc_pack
 
 set_instance_cfg() {
-    sed "s/pack-name/$(toml get -r "$2" name)$4/g" $TEMPLATES/instance.cfg |
+    sed "s/pack-name/$(toml get -r "$2" name)/g" $TEMPLATES/instance.cfg |
         sed "s|url-placeholder|$3|g" |
         sed "s/pack-location/$(remove_dotslash "$1")/g" >$CURRENT/instance.cfg
-    # echo -n ''
 }
 export -f set_instance_cfg
 
 create_zip() {
     cd download/current_instance/ &&
-        zip -r -q "../$1.zip" ./* &&
-        zip -r -q "../$1.zip" .minecraft/* &&
+        zip -r "../$1.zip" ./* &&
+        zip -r "../$1.zip" .minecraft/* &&
         cd ../..
 }
 export -f create_zip
@@ -55,7 +54,7 @@ find . -type d -exec test -e '{}'/pack.toml \; \
     -exec bash -c 'set_mmc_pack $1' bash {}/pack.toml \; \
     -exec bash -c 'set_instance_cfg $1 $2 $3' bash {} {}/pack.toml "$REPO_URL" \; \
     -exec bash -c 'create_zip $1' bash {} \; \
-    -exec bash -c 'set_instance_cfg $1 $2 $3 $4' bash {} {}/pack.toml $LOCAL_URL "-Local"\; \
+    -exec bash -c 'set_instance_cfg $1 $2 $3' bash {} {}/pack.toml $LOCAL_URL \; \
     -exec bash -c 'create_zip $1' bash {}-local \; \
     -exec bash -c 'echo "$1 processed succesfully."' bash {} \;
 
